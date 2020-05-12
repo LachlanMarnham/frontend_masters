d97a78dda33a3c77240ea5ca834141486abb73c9
Lachlan Marnham <lachlan.marnham@farfetch.com>
2020-05-12 16:24:20 +0100

    Move arraysEqual to module utils.js

--- computer_science/1/big_o.js
+++ computer_science/1/big_o.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var utils = require('../utils');
 
 
 function crossAdd(input_vals) {
@@ -19,29 +20,6 @@ function crossAdd(input_vals) {
     }
 
     return result;
-}
-
-
-function arraysEqual(arr_1, arr_2) {
-    /* Returns true if all elements in arr_1 are equal to the corresponding (index-wise) element of arr_2.
-    Short-circuits if arrays have different lengths. Also works with arrays of arrays. */
-    
-    if (arr_1.length !== arr_2.length) {
-        return false;
-    }
-
-    for (let i = 0; i < arr_1.length; i++) {
-        let el_1 = arr_1[i];
-        let el_2 = arr_2[i];
-
-        if (Array.isArray(el_1) && Array.isArray(el_2)) {
-            return arraysEqual(el_1, el_2);
-        } else if (el_1 !== el_2) {
-            return false;
-        }
-    }
-
-    return true;
 }
 
 
@@ -86,11 +64,11 @@ function makeTuples(input_vals) {
 
 
 if (require.main === module) {
-    assert(arraysEqual(crossAdd([1, 2, 3]), [4, 4, 4]));
-    assert(arraysEqual(crossAdd([3, 4, 7, 13]), [16, 11, 11, 16]));
+    assert(utils.arraysEqual(crossAdd([1, 2, 3]), [4, 4, 4]));
+    assert(utils.arraysEqual(crossAdd([3, 4, 7, 13]), [16, 11, 11, 16]));
 
     assert(find(1, [1, 2, 3]) === true);
     assert(find(4, [5, 6, 7]) === false);
 
-    assert(arraysEqual(makeTuples([1, 2]), [[1, 1], [1, 2], [2, 1], [2, 2]]));
+    assert(utils.arraysEqual(makeTuples([1, 2]), [[1, 1], [1, 2], [2, 1], [2, 2]]));
 }
